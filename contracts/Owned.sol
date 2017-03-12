a683bee92879fb294d97e993500db1b9f7752f7a
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2017-03-12 07:54:25 +0000

    Added function to transfer ownership of GitHubOracle

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -1,11 +1,17 @@
 pragma solidity ^0.4.8;
 
 contract Owned {
-
     address public owner= msg.sender;
+    event NewOwner(address owner);
 
     modifier only_owner {
         if (msg.sender != owner) throw;
         _;
+    }
+    
+    function setOwner(address _owner)
+     only_owner {
+        owner = _owner;
+        NewOwner(_owner);
     }
 }
